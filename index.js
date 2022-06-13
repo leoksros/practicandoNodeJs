8a612f2d490c19bce1e322e3de5df705725d679e
Leo <Leonardo>
2022-06-13 01:37:50 -0300

    create, read and delete dj added

--- index.js
+++ index.js
@@ -1,27 +1,85 @@
 let djs = [
     {
+        id: 1,
         name: 'John',
         surname: 'Digweed',
         genre: 'Techno'
     },
     {
+        id: 2,
         name: 'Nick',
         surname: 'Warren',
         genre: 'Progressive House'
     }
 ];
 
+//const http = require('http'); //importando modulo http para hacer requests
 
-const http = require('http'); //importando modulo http para hacer requests
-
-const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+/* const app = http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(JSON.stringify(djs));
+}); */
+
+const express = require('express');
+const app = express();
+app.use(express.json()); //parsea jsons
+
+app.get('/', (request,response) => {
+    response.send('<h1>node.js</h1>');
 });
 
+app.get('/api/djs', (request,response) => {
+    response.json(djs);
+});
+
+app.get('/api/djs/:id', (request,response) => {
+    const id = Number(request.params.id);
+    const dj = djs.find(dj => dj.id == id);
+    if(dj){
+        response.json(dj);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete('/api/djs/:id', (request,response) => {
+    const id = Number(request.params.id);
+    djs = djs.filter(dj => dj.id !== id);
+    response.json(djs);
+});
+
+app.post('/api/djs', (request,response) => {
+    const dj = request.body;
+    
+    if(!dj || !dj.name) {
+        return response.status(400).json({
+            error: 'dj.name is missing!'
+        })
+    }
+    if(!dj || !dj.surname) {
+        return response.status(400).json({
+            error: 'dj.surname is missing!'
+        })
+    }
+
+    const idDjs = djs.map(dj => dj.id);
+    const maxId = Math.max(... idDjs);
+    const newDj = {
+        id: maxId + 1,
+        name: dj.name,
+        surname: dj.surname,
+        genre: dj.genre !== 'undefined' ? dj.genre : 'no-value'
+    }
+
+    djs = [... djs, newDj]; // djs.concat(newDj);
+
+    response.json(newDj);
+})
+
 const PORT = 3001;
-app.listen(PORT); //el servidor escucha el PORT
-console.log(`Server running on port ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}); //el servidor escucha el PORT
 
 
 
